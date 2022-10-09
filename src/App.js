462fb64a06691d01ac3ead23cfb2a8ccb84d77fd
Scott Brass <slcscooter@gmail.com>
2022-10-08 20:30:55 -0700

    adding game modes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import { GameField } from "./components/GameField";
+import { GameSelector } from "./components/GameSelector";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <GameField />
-      </header>
-    </div>
-  );
+  const [game, setGame] = useState("CHOOSING_GAME");
+
+  function handleOnSubmitMinus() {
+    setGame("MINUS");
+  }
+  function handleOnSubmitMultiply() {
+    setGame("MULTIPLY");
+  }
+  function handleOnSubmitPlus() {
+    setGame("PLUS");
+  }
+
+  if (game !== "CHOOSING_GAME") {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <GameField />
+        </header>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <GameSelector
+            handleOnSubmitMinus={handleOnSubmitMinus}
+            handleOnSubmitMultiply={handleOnSubmitMultiply}
+            handleOnSubmitPlus={handleOnSubmitPlus}
+          />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
