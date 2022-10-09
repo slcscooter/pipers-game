8487aa11340ede435930d485a58e5ab073e333fd
Scott Brass <slcscooter@gmail.com>
2022-10-08 23:39:02 -0700

    added multiply and subtract game modes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
 import "./App.css";
-import React, { useState } from "react";
-import { GameField } from "./components/GameField";
-import { GameSelector } from "./components/GameSelector";
+import React from "react";
+import { GameLayout } from "./components/GameLayout";
 
 function App() {
-  const [game, setGame] = useState("CHOOSING_GAME");
-
-  function handleOnSubmitMinus() {
-    setGame("MINUS");
-  }
-  function handleOnSubmitMultiply() {
-    setGame("MULTIPLY");
-  }
-  function handleOnSubmitPlus() {
-    setGame("PLUS");
-  }
-
-  if (game !== "CHOOSING_GAME") {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <GameField />
-        </header>
-      </div>
-    );
-  } else {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <GameSelector
-            handleOnSubmitMinus={handleOnSubmitMinus}
-            handleOnSubmitMultiply={handleOnSubmitMultiply}
-            handleOnSubmitPlus={handleOnSubmitPlus}
-          />
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <GameLayout />
+      </header>
+    </div>
+  );
 }
 
 export default App;
